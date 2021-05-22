72a4a5acf7842f7a155436dd9ba2d0466bccc2e2
harrisonblackburn <blackbhr@yahoo.com>
2021-05-22 14:09:28 -0500

    trying to deploy to heroku using atlas

--- Develop/models/Workout.js
+++ Develop/models/Workout.js
@@ -40,7 +40,7 @@ const workoutSchema = new Schema(
     },
     {
       toJSON: {
-        // include any virtual properties when data is requested
+        
         virtuals: true
       }
     }
