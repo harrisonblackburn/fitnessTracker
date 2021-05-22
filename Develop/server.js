9e68f0183ba6b20a82539c0071245f8ae2ec1a97
harrisonblackburn <blackbhr@yahoo.com>
2021-05-21 21:16:05 -0500

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -16,9 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true,  useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts", { useNewUrlParser: true,  useUnifiedTopology: true,
 useCreateIndex: true,
-useFindAndModify: false    });
+useFindAndModify: false });
 
 app.use(routes)
 
