539aaa88b792b3394fcb26e4370dbd54c6f71ee1
harrisonblackburn <blackbhr@yahoo.com>
2021-05-21 20:58:35 -0500

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -16,7 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true,  useUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false    });
 
 app.use(routes)
 
